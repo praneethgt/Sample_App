7e131b1a372fada74c5007dbc3df57c4adb78db3
Praneeth Vipparty <praneeth.vipparty@outlook.com>
2012-08-10 20:37:07 +0530

    Intial Release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '07972aa777f448fab2bc208b5a190745ce444c9218dbdc6731999887033ff14d9b8d8244a2e73755da8638be6839dfcd42288916d335fff80210af99e4762f80'
